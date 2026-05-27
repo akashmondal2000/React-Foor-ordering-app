3473c5e56f38881e99b4018dfd7c76ad023a39bd
akashmondal2000 <mondalakash494@gmail.com>
2026-05-28 02:47:09 +0530

    crate formatter for currency,add button coponent

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useState } from "react";
+import MealItem from "./MealItem.jsx";
 
 const Meals = () => {
   const [loadedMeals, setloadedMeals] = useState([]);
 
-  
   useEffect(() => {
-      async function fetchMeals() {
-        const responce = await fetch("http://localhost:3000/meals");
-    
-        if (!responce.ok) {
-          //.... status code 400/500
-        }
-    
-        const meals = await responce.json();
-        setloadedMeals(meals);
-    
+    async function fetchMeals() {
+      const responce = await fetch("http://localhost:3000/meals");
+
+      if (!responce.ok) {
+        //.... status code 400/500
       }
-      fetchMeals()
+
+      const meals = await responce.json();
+      setloadedMeals(meals);
+    }
+    fetchMeals();
   }, []);
 
-  console.log(loadedMeals)
-  return <ul id="meals">{
-    loadedMeals.map((meal)=>(<li key={meal.id}>{meal.name}</li>))
-  }</ul>;
+  console.log(loadedMeals);
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 };
 
 export default Meals;
---
+++ src/util/formatter.js
@@ -0,0 +1,4 @@
+export const currencyFormater = Intl.NumberFormat("en-US",{
+    style:"currency",
+    currency:"USD"
+})

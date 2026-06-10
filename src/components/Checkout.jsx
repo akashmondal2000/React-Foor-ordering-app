8b726e9acd8d401861c1d692b2584b2d7d382492
akashmondal2000 <mondalakash494@gmail.com>
2026-06-10 09:09:45 +0530

    send form data to the backend using post method, create a custom hook for sending http request

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -14,26 +14,49 @@ const Checkout = () => {
     0,
   );
 
-  function handleClose(){
+  function handleClose() {
     userProgressCtx.hideCheckout();
-  };
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    /* convert formData object to a sompler js Object*/
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body:JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        }
+      })
+    });
+  }
 
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'}>
-      <form>
+    <Modal open={userProgressCtx.progress === "checkout"}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormater.format(cartTotal)} </p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
-            <Input label="Postal Code" type="text" id="postal-code"/>
-            <Input label="city" type="text" id="city"/>
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="city" type="text" id="city" />
         </div>
 
         <p className="modal-actions">
-            <Button onClick={handleClose} type="button" textOnly>Close</Button>
-            <Button>Subimt Order</Button>
+          <Button onClick={handleClose} type="button" textOnly>
+            Close
+          </Button>
+          <Button>Subimt Order</Button>
         </p>
       </form>
     </Modal>

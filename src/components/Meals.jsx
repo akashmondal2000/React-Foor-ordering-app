325c6b0184745fc53a0300bbda05413262e79640
akashmondal2000 <mondalakash494@gmail.com>
2026-06-10 12:15:31 +0530

    create a error component, and handle error state

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import MealItem from "./MealItem.jsx";
 import useHttp from "../hook/useHttp.js";
+import Error from "./Error.jsx";
 
 const requestConfig = {};
 
@@ -12,7 +13,11 @@ const Meals = () => {
   
   
   if(isLoading){
-    return <p>Fetching meals.....</p>;
+    return <p className="center"> Fetching meals.....</p>;
+  }
+
+  if(error){
+    return <Error title="faild to fetch meals" message={error} /> 
   }
 
   return (
